076143a7f06afcdf9d51c6708b121d326231ef32
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2022-12-14 08:53:34 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,13 +10,20 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit
+	
+	if (n >= 0)
 
-	if (last_digit < 0)
 	{
-		last_digit = -last_digit;
+		last_digit= n % 10;
+	}
+	
+	else (last_digit < 0)
+	{
+		last_digit = (n % 10) * -1;
 	}
 
-	printf("%d", last_digit);
+	_putchar('0' + last_digit);
 	return (last_digit);
 }
+
