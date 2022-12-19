0bf34be2c4ad20647f6422a412b35439e9692477
tonyrex99 <ebunifeoluwaadenuga@yahoo.com>
2022-12-19 17:15:51 +0100

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,22 @@
 
 void rev_string(char *s)
 {
-	int len = _strlen(s);
+	int length_of_string;
+
+	length_of_string = 0;
+
+	while (*(s + length_of_string) != '\0')
+	{
+		length_of_string++;
+	}
+
 	int i;
 	char temp;
 
-	for (i = 0; i < len / 2; i++)
+	for (i = 0; i < length_of_string / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		s[i] = s[length_of_string - i - 1];
+		s[length_of_string - i - 1] = temp;
 	}
 }
