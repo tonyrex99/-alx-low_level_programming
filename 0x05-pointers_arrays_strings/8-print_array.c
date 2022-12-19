fde22ef2565efef17094ad91d2ecfd9847339c90
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2022-12-19 18:22:52 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,13 +7,16 @@
  */
 void print_array(int *a, int n)
 {
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", a[i]);
-        if (i < n - 1)
-            printf(", ");
-            printf(" ");
-    }
-    _putchar('\n');
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
 }
