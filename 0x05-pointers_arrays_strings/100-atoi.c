18fa8f2439d11ca92d1945dbcbd9513156751094
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2022-12-19 18:48:17 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,33 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
+	int index, ind2;
+	unsigned int res;
 	int sign = 1;
-	int i = 0;
+	char now;
 
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	index = 0;
+	res = 0;
+	while (*(s + index) != '\0')
 	{
-		if (s[i] == '-')
+		now = *(s + index);
+		if (now == '-')
+		{
 			sign *= -1;
-		i++;
+		}
+		if (now >= '0' && now <= '9')
+		{
+			ind2 = index;
+			while (*(s + ind2) > 47 && *(s + ind2) < 58)
+			{
+				res = (res * 10) + *(s + ind2) - '0';
+				ind2++;
+			}
+			break;
+		}
+		index++;
 	}
-
-	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
-
-	return (result * sign);
+	if (sign < 0)
+		res *= sign;
+	return (res);
 }
