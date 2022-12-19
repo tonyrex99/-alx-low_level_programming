c93052271283e8e4ed7a896b3eaadf43e2ee200c
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2022-12-19 18:28:50 +0100

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+/**
+ * _strcpy - Copy a string to a buffer
+ * @dest: The buffer to copy the string to
+ * @src: The string to copy
+ *
+ * Return: A pointer to the destination buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *p = dest;
+	while (*src != '\0')
+		*p++ = *src++;
+	*p = '\0';
+	return dest;
+}
