139b7ea58f67616aa9949774d7c46424fc519a6b
tonyrex99 <ebunifeoluwaadenuga@yahoo.com>
2022-12-23 01:49:43 +0100

    betty compliance fixed for length 9

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,8 @@
  * @s2: The second string
  *
  * Return: An integer less than, equal to, or greater than zero
- * if s1 is found, respectively, to be less than, to match, or be greater than s2.
+ * if s1 is found, respectively, to be less than,
+ * to match, or be greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
