27089e84fcc648e8e669cf7a2db4ee1383c1ec6e
tony <ebunifeoluwaadenuga@gmail.com>
2023-01-03 05:13:55 +0000

    fdf

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - finds the occurence of a char c
+ * @s: string
+ * @c: char
+ *
+ * Return: pointers to occurence of c or NULL if none
+ */
+
+char *
+_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c) return (s);
+		s++;
+	}
+
+	/* test for '\0' */
+	if (*s == c) return (s);
+
+	return (NULL);
+}
