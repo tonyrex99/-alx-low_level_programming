de0bc01bbfa6de69b2055232d1c55acc9f9aecfc
tony <ebunifeoluwaadenuga@gmail.com>
2023-01-03 04:53:19 +0000

    betty compliant

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcmp - Compare two strings
  * @s1: The first string
@@ -9,12 +8,12 @@
  * if s1 is found, respectively, to be less than,
  * to match, or be greater than s2.
  */
-int _strcmp(char *s1, char *s2)
+int
+_strcmp(char *s1, char *s2)
 {
 	int i;
 
 	for (i = 0; s1[i] == s2[i] && s1[i] != '\0'; i++)
 		;
-
 	return (s1[i] - s2[i]);
 }
