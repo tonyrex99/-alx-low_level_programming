1b80adf014a4723a88b763a838c18b65bf849aec
tony <ebunifeoluwaadenuga@gmail.com>
2023-01-03 05:19:27 +0000

    dfd

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,15 @@
-include "main.h"
+#include "main.h"
 
-	/**
-	 * _memcpy - copy n bytes from memory area to source
-	 * @dest: destination
-	 * @src: source
-	 * @n: number of bytes
-	 *
-	 * Return: pointers to s
-	 */
+/**
+ * _memcpy - copy n bytes from memory area to source
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ *
+ * Return: pointers to s
+ */
 
-	char *
-	_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,17 +9,18 @@
  * Return: pointers to occurence of c or NULL if none
  */
 
-char *
-_strchr(char *s, char c)
+char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == c) return (s);
+		if (*s == c)
+			return (s);
 		s++;
 	}
 
 	/* test for '\0' */
-	if (*s == c) return (s);
+	if (*s == c)
+		return (s);
 
 	return (NULL);
 }
