e8f697c01fa706fd103f60bb0f49138fa9fa7b33
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2023-01-13 13:32:07 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,38 +6,41 @@
  * @argv: args
  * Return: RETURNS ZERO
  */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	if (argc != 3)
 	{
 		errors();
-		return 98;
+		return (98);
 	}
 
 	if (!is_digit(argv[1]) || !is_digit(argv[2]))
 	{
 		errors();
-		return 98;
+		return (98);
 	}
 
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[2]);
 	int result = num1 * num2;
+
 	printf("%d\n", result);
-	return 0;
+	return (0);
 }
 
 int is_digit(char *s)
 {
 
 	int len = _strlen(s);
+
 	for (int i = 0; i < len; i++)
 	{
 		if (!isdigit(s[i]))
 		{
-			return 0;
+			return (0);
 		}
 	}
-	return 1;
+return (1)	;
 }
 
 void errors(void)
