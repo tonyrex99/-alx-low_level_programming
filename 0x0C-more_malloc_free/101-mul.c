a43befb472c920598a0e80d0bc0863f71e5fe2bb
tonyrex99 <59386507+tonyrex99@users.noreply.github.com>
2023-01-13 10:51:33 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,22 +4,26 @@
  * main - multiplies two positive numbers
  * @argc: n arguments
  * @argv: args
- * Return: int
+ * Return: RETURNS ZERO
  */
 int main(int argc, char *argv[])
 {
 unsigned long mul;
 int i, j;
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
 
 	}
